19be734bf26de7ef5a1328f0da01e8f7426b031e
magi <magi@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2005-01-24 10:12:36 +0000

    added integrate 2d code

---
+++ glimmer-cism/erosion/trunk/src/erosion_integrate2d.f90
@@ -0,0 +1,180 @@
+! erosion_integrate2d.f90
+! Magnus Hagdorn, March 2003
+!
+! integrate a function defined on a regular cartesian grid over arbitrary rectangular boundaries
+
+module erosion_integrate2d
+  use geometry
+
+type(geom_poly), private :: cell
+private :: addmat
+real :: ismintegrate2d_zero = 0.0
+
+contains
+  subroutine init_integrate2d
+    implicit none
+
+    cell = poly_new(4)
+  end subroutine init_integrate2d
+
+  subroutine calc_weight(coords, weight, patch, n)
+    use sparse
+    use geometry
+    use glimmer_coordinates
+    implicit none
+    type(coordsystem), intent(in) :: coords
+    type(sparse_matrix) :: weight            ! sparse matrix
+    type(geom_poly), intent(in) :: patch     ! the polygone over which will be integrated
+    integer, intent(in) :: n                 ! cell number
+
+    ! local variables
+    type(geom_ipoint) :: node
+    integer i
+    integer imin, inum, pow, jmin, jnum,  num
+    real w
+
+    ! find the bounding box
+    node = coordsystem_get_node(coords,patch%poly(1))
+    imin = node%pt(1)
+    inum = node%pt(1)
+    jmin = node%pt(2)
+    jnum = node%pt(2)
+    do i=2,patch%n
+       node = coordsystem_get_node(coords,patch%poly(i))
+       if (node%pt(1).lt.imin) imin = node%pt(1)
+       if (node%pt(1).gt.inum) inum = node%pt(1)
+       if (node%pt(2).lt.jmin) jmin = node%pt(2)
+       if (node%pt(2).gt.jnum) jnum = node%pt(2)
+    end do
+    node%pt(1) = imin 
+    node%pt(2) = jmin 
+    inum = inum - imin
+    jnum = jnum - jmin
+
+    num = max(inum,jnum)
+
+    if (num.eq.0) then ! trivial case shape is within single cell
+       w = 0.5*abs(poly_area2(patch))/(coords%delta%pt(1)*coords%delta%pt(2))
+       call addmat(coords,weight,n,node,w)
+       return
+    end if
+    
+    pow = ceiling(log(real(num))/log(2.)) 
+    if (pow.ge.1) then ! can we halve the area?
+       pow = pow - 1
+    end if
+
+    ! non-trival case, start dividing
+    num = 2**pow
+    call divide_weight(coords,weight,patch,node,pow,n)
+    node%pt(1) = node%pt(1)+num
+    call divide_weight(coords,weight,patch,node,pow,n)
+    node%pt(2) = node%pt(2)+num
+    call divide_weight(coords,weight,patch,node,pow,n)
+    node%pt(1) = node%pt(1)-num
+    call divide_weight(coords,weight,patch,node,pow,n)
+    node%pt(2) = node%pt(2)-num
+  end subroutine calc_weight
+
+  recursive subroutine divide_weight(coords,weight,patch,node,pow,n)
+    use sparse
+    use glimmer_coordinates
+    implicit none
+    type(coordsystem), intent(in) :: coords
+    type(sparse_matrix) :: weight            ! sparse matrix
+    type(geom_poly), intent(in) :: patch     ! the polygone over which will be integrated
+    type(geom_ipoint),intent(inout) :: node  ! cell coordinates
+    integer, intent(in) :: pow               ! size of square (numi,numj = 2**pow)
+    integer, intent(in) :: n                 ! cell number
+
+    ! local variables
+    type(geom_poly) :: poly
+    type(geom_point) :: point
+    type(geom_ipoint) :: n2
+    integer p
+    logical inters
+    integer i,j, i1,j1,next
+    real val
+
+    ! set up current cell coords
+    cell%n=4
+    point = coordsystem_get_coord(coords,node)
+    point%pt(:) = point%pt(:) -0.5*coords%delta%pt(:)
+    cell%poly(1) = point
+    
+    point%pt(1) = point%pt(1) + coords%delta%pt(1)*(2**pow)
+    cell%poly(2) = point
+
+    point%pt(2) = point%pt(2) + coords%delta%pt(2)*(2**pow)
+    cell%poly(3) = point
+
+    point%pt(1) = point%pt(1) - coords%delta%pt(1)*(2**pow)
+    cell%poly(4) = point
+
+    inters = .false.
+    outer: do j=1,4 ! loop over box coords
+       j1 = mod(j+2,4)+1
+       do  i = 1,patch%n ! loop over patch coords
+          i1 = mod(i+patch%n-2,patch%n)+1
+          inters = intersectProp(cell%poly(j1),cell%poly(j), patch%poly(i1), patch%poly(i))
+          if (inters) exit outer
+       end do
+    end do outer
+
+    if (.not.inters) then ! patch does not intersect cell
+       ! two possibilities
+       if (point_in_cpoly(cell%poly(1),patch)) then  ! cell is inside patch
+          do j = node%pt(2),node%pt(2)+2**pow-1
+             do i = node%pt(1),node%pt(1)+2**pow-1
+                n2%pt(1) = i
+                n2%pt(2) = j
+                call addmat(coords,weight,n,n2,1.0)
+             end do
+          end do
+          return   
+       else ! cell is outside patch
+          return
+       end if
+    end if
+
+    ! shape intersects box
+    if (pow .gt. 0) then  ! we can still divide
+       p = pow - 1
+       next = 2**p
+       call divide_weight(coords,weight,patch,node,p,n)
+       node%pt(1) = node%pt(1) + next
+       call divide_weight(coords,weight,patch,node,p,n)
+       node%pt(2) = node%pt(2) + next
+       call divide_weight(coords,weight,patch,node,p,n)
+       node%pt(1) = node%pt(1) - next
+       call divide_weight(coords,weight,patch,node,p,n)
+       node%pt(2) = node%pt(2) - next
+       return
+    else ! we are left with a single cell now
+       ! calculate intersection between cell and patch
+       poly = ConvexIntersect(patch,cell)
+       val = 0.5*real(poly_area2(poly)) / (coords%delta%pt(1)*coords%delta%pt(2))
+       call addmat(coords,weight,n,node,val)
+       call poly_delete(poly)
+    end if
+          
+
+  end subroutine divide_weight
+
+  subroutine addmat(coords,weight,n,node,val)
+    ! adding val if i,j inside region, ignore otherwise
+    use glimmer_coordinates
+    use sparse
+    implicit none
+    type(coordsystem), intent(in) :: coords
+    type(sparse_matrix) :: weight
+    integer, intent(in) :: n
+    type(geom_ipoint), intent(in) :: node
+    real, intent(in) :: val
+
+    if (coordsystem_node_inside(coords,node) .and. abs(val).gt.ismintegrate2d_zero) then
+       call add_to_matrix(weight,n,coordsystem_linearise2d(coords,node),val)
+    end if
+  end subroutine addmat
+
+end module erosion_integrate2d
---
+++ glimmer-cism/erosion/trunk/src/glimmer_coordinates.f90
@@ -0,0 +1,173 @@
+! glimmer_coordinates.f90
+! Magnus Hagdorn, April 2003
+!
+! module for handling regular coordinate systems
+
+module glimmer_coordinates
+  use geometry
+
+  ! define unit for error_logs
+  integer :: coordinates_unit = 6
+
+  ! new type describing coordinate systems
+  type coordsystem
+     type(geom_point) :: origin   ! origin of coordinate space
+     type(geom_point) :: delta    ! stepsize in x and y direction
+     type(geom_ipoint) :: size    ! extent in x and y direction
+  end type coordsystem
+  
+  ! interface of creating new coord system
+  interface coordsystem_new
+     module procedure coordsystem_new_real, coordsystem_new_pt
+  end interface
+
+contains
+  subroutine coordsystem_print(coord, unit)
+    ! print coordsystem info to unit
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    integer unit
+    write(unit,*) 'Origin ',coord%origin%pt
+    write(unit,*) 'Delta  ',coord%delta%pt
+    write(unit,*) 'Size   ',coord%size%pt
+  end subroutine coordsystem_print
+
+  function coordsystem_new_real(ox, oy, dx, dy, sx, sy)
+    ! create new coordinate system from individual variables
+    implicit none
+    real, intent(in) :: ox, oy, dx, dy
+    integer, intent(in) :: sx, sy
+    type(coordsystem) :: coordsystem_new_real
+    
+    ! origin
+    coordsystem_new_real%origin%pt(1) = ox
+    coordsystem_new_real%origin%pt(2) = oy
+    ! deltas
+    coordsystem_new_real%delta%pt(1) = dx
+    coordsystem_new_real%delta%pt(2) = dy
+    ! size
+    coordsystem_new_real%size%pt(1) = sx
+    coordsystem_new_real%size%pt(2) = sy
+  end function coordsystem_new_real
+
+  function coordsystem_new_pt(o, d, s)
+    ! create new coordinate system from points
+    implicit none
+    type(geom_point), intent(in) :: o, d
+    type(geom_ipoint), intent(in) :: s
+    type(coordsystem) :: coordsystem_new_pt
+
+    ! origin
+    coordsystem_new_pt%origin = o
+    ! deltas
+    coordsystem_new_pt%delta = d
+    ! size
+    coordsystem_new_pt%size = s
+  end function coordsystem_new_pt
+
+  function coordsystem_get_coord(coord,node)
+    ! get coordinates of node
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    type(geom_ipoint), intent(in) :: node
+
+    type(geom_point) :: coordsystem_get_coord
+  
+#ifdef DEBUG_COORDS
+    if (.not.coordsystem_node_inside(coord,node)) then
+       write(coordinates_unit,*) 'Error (',__FILE__,__LINE__,'): node (',node%pt,') not inside coord system'
+       call coordsystem_print(coord,coordinates_unit)
+       stop
+    end if
+#endif
+
+    coordsystem_get_coord%pt(:) = coord%origin%pt(:) + (node%pt(:) - 1)*coord%delta%pt(:)
+  end function coordsystem_get_coord
+
+  function coordsystem_get_node(coord,point)
+    ! get index of nearest node given coords of a point
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    type(geom_point), intent(in) :: point
+    
+    type(geom_ipoint) :: coordsystem_get_node
+    
+    coordsystem_get_node%pt(:) = 1+floor(0.5+(point%pt(:)-coord%origin%pt(:))/coord%delta%pt(:))
+    if (coordsystem_get_node%pt(1).eq.coord%size%pt(1)+1) coordsystem_get_node%pt(1) = coord%size%pt(1)
+    if (coordsystem_get_node%pt(2).eq.coord%size%pt(2)+1) coordsystem_get_node%pt(2) = coord%size%pt(2)
+
+#ifdef DEBUG_COORDS
+    if (.not.coordsystem_node_inside(coord,coordsystem_get_node)) then
+       write(coordinates_unit,*) 'Error (',__FILE__,__LINE__,'): point (',point%pt,') not inside coord system'
+       call coordsystem_print(coord,coordinates_unit)
+       stop
+    end if
+#endif
+  end function coordsystem_get_node
+
+  function coordsystem_node_inside(coord,node)
+    ! return true iff node is inside coord system
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    type(geom_ipoint), intent(in) :: node
+
+    logical coordsystem_node_inside
+    
+    coordsystem_node_inside = (all(node%pt.ge.1) .and. all(node%pt.le.coord%size%pt))
+  end function coordsystem_node_inside
+
+  function coordsystem_point_inside(coord,point)
+    ! return true iff point is inside coord system
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    type(geom_point), intent(in) :: point
+    logical coordsystem_point_inside
+    integer i
+
+    coordsystem_point_inside = .true.
+    do i=1,geom_dim
+       coordsystem_point_inside = (point%pt(i).ge.coord%origin%pt(i)) .and. &
+            (point%pt(i).le.coord%origin%pt(i)+coord%size%pt(i)*coord%delta%pt(i))
+       if (.not.coordsystem_point_inside) then
+          exit
+       end if
+    end do
+  end function coordsystem_point_inside
+    
+  function coordsystem_linearise2d(coord,node)
+    ! linearise node, given coord
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    type(geom_ipoint), intent(in) :: node
+    integer coordsystem_linearise2d
+
+    coordsystem_linearise2d = -1
+
+#ifdef DEBUG_COORDS
+    if (.not.coordsystem_node_inside(coord,node)) then
+       write(coordinates_unit,*) 'Error (',__FILE__,__LINE__,'): node (',node%pt,') not inside coord system'
+       return
+    end if
+#endif
+    
+    coordsystem_linearise2d = node%pt(1) + (node%pt(2)-1)*coord%size%pt(1)
+  end function coordsystem_linearise2d
+
+  function coordsystem_delinearise2d(coord, ind)
+    ! expand linearisation
+    implicit none
+    type(coordsystem), intent(in) :: coord
+    integer, intent(in) :: ind
+    type(geom_ipoint) :: coordsystem_delinearise2d
+
+#ifdef DEBUG_COORDS
+    if (ind.lt.1 .or. ind.gt.coord%size%pt(1)*coord%size%pt(2)) then
+       write(coordinates_unit,*) 'Error (',__FILE__,__LINE__,'): index ',ind,' outside coord system'
+       stop
+    end if
+#endif
+
+    coordsystem_delinearise2d%pt(1) = mod(ind-1,coord%size%pt(1)) + 1
+    coordsystem_delinearise2d%pt(2) = (ind-1)/coord%size%pt(1) + 1
+  end function coordsystem_delinearise2d
+end module glimmer_coordinates
