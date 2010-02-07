89c786202927fb600360fc05297fbd6269b58dfd
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-02-07 00:40:50 +0000

    major changes to shelf_driver.F90

--- glimmer-cism-lanl/branches/plume_integration/src/fortran/shelf_driver.F90
+++ glimmer-cism-lanl/branches/plume_integration/src/fortran/shelf_driver.F90
@@ -1,66 +1,39 @@
-! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-! +                                                           +
-! +  shelf_driver.F90 - part of the GLIMMER ice model         + 
-! +                                                           +
-! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-! 
-! Copyright (C) 2004 GLIMMER contributors - see COPYRIGHT file 
-! for list of contributors.
-!
-! This program is free software; you can redistribute it and/or 
-! modify it under the terms of the GNU General Public License as 
-! published by the Free Software Foundation; either version 2 of 
-! the License, or (at your option) any later version.
-!
-! This program is distributed in the hope that it will be useful, 
-! but WITHOUT ANY WARRANTY; without even the implied warranty of 
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-! GNU General Public License for more details.
-!
-! You should have received a copy of the GNU General Public License 
-! along with this program; if not, write to the Free Software 
-! Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 
-! 02111-1307 USA
-!
-! GLIMMER is maintained by:
-!
-! Ian Rutt
-! School of Geographical Sciences
-! University of Bristol
-! University Road
-! Bristol
-! BS8 1SS
-! UK
-!
-! email: <i.c.rutt@bristol.ac.uk> or <ian.rutt@physics.org>
-!
-! GLIMMER is hosted on NeSCForge:
-!
-! http://forge.nesc.ac.uk/projects/glimmer/
-!
-! +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 #ifdef HAVE_CONFIG_H
 #include "config.inc"
 #endif
 
+#include "glide_mask.inc"
+
 program shelf_driver
   !*FD This is a simple GLIDE driver. It can be used to run
   !*FD a Petermann shelf simulation
-  use glimmer_global, only:rk
+  use glimmer_global, only:rk,sp
+!  use glimmer_global, only : sp
+
   use glide
-  use shelf_forcing
+  use glide_mask
+ ! use shelf_forcing
  ! use glimmer_log
  ! use glimmer_config
   use glimmer_commandline
   use glimmer_writestats_module
-  use plume
+  use plume, only:plume_iterate,plume_initialise,plume_finalise
   use plume_io
-  use plume_global, only:bpos
+  
+
+  use plume_global, only: n_grid
 
   use glimmer_paramets, only: thk0
+  use glide_temp, only: timeevoltemp
 
   implicit none
+
+  type shelf_climate
+     ! holds parameters for the shelf climate
+     real(kind=sp) :: artm = 0.0
+     real(kind=sp) :: accumulation_rate = 0.0
+     real(kind=sp) :: eus = 0.0
+  end type shelf_climate
 
   type(glide_global_type) :: model        ! model instance
   type(shelf_climate) :: climate_cfg      ! climate configuration info
@@ -68,8 +41,11 @@ program shelf_driver
   real(kind=rk) :: time
   real(kind=dp) :: t1,t2
   integer :: clock,clock_rate
-  character(len=512) :: plume_nl
-  logical :: plume_suppress_output
+  integer :: USE_PLUME = 1
+  character(len=512) :: plume_nl,plume_output_nc_file,plume_output_prefix,plume_ascii_output_dir
+  logical :: plume_suppress_ascii_output,plume_suppress_logging,plume_write_all_states
+  real(kind=dp) :: plume_min_subcycle_time,plume_min_spinup_time,plume_steadiness_tol
+  integer :: plume_imin,plume_imax,plume_kmin,plume_kmax
 
   call glimmer_GetCommandline()
   
@@ -87,92 +63,169 @@ program shelf_driver
   call glide_config(model,config)
 
   ! read [plume] section of glimmer config file
-  call plume_read_print_config(model,config,plume_nl,plume_suppress_output)
-!  model%options%which_bmlt = 1 !use plume
-  model%options%which_bmlt = 0 !use plume
-	 	
+  call plume_read_print_config(model,config,&
+                               plume_nl,&
+                               plume_ascii_output_dir, &
+                               plume_output_prefix, &
+                               plume_output_nc_file, &
+                               plume_suppress_ascii_output,&
+                               plume_suppress_logging)
+
   ! config the [Peterman shelf] section of the config file
   call shelf_config_initialise(climate_cfg,config)
+
+  call CheckSections(config)
 
   !initialise glide
   call glide_initialise(model)
 
-  ! this is a hack to get the lower surface into glide
-  ! without changing the code that reads in initial data
-  model%geometry%lsrf(:,:) = -1.0*920.0/1030.0 * model%geometry%thck(:,:) / thk0 
-  model%geometry%topg = -2000.0/thk0
-
-  call CheckSections(config)
+  !initialise climate in model
+  model%climate%acab(:,:) = climate_cfg%accumulation_rate
+  model%climate%artm(:,:) = climate_cfg%artm
+  model%climate%eus = climate_cfg%eus 
+  
+  ! this just sets the ice column temperature to the ambient air temperature
+  call timeevoltemp(model,0)
 
   ! fill dimension variables
   call glide_nc_fillall(model)
 
   time = model%numerics%tstart
 
-  call spinup_lithot(model)
+ if (model%options%which_bmlt .eq. USE_PLUME) then
+    ! we are using the plume
+     
+	plume_kmin = 1
+	plume_kmax = model%general%nsn - 4  !hack for now 
+					    ! should figure this out from the thickness field
+	plume_imin = 1
+	plume_imax = model%general%ewn
+     
+     call plume_logging_initialize(trim(plume_ascii_output_dir), &
+                                   trim(plume_output_prefix), &
+                                    plume_suppress_logging)
 
-!  call plume_io_initialize('/home/gladish/computation/glimmer-cism/test_run/', &
-!                           '999',.false.)
+     call plume_initialise(trim(plume_nl), &
+                           plume_suppress_ascii_output, &
+                           plume_suppress_logging,&
+                           model%geometry%lsrf * thk0,&
+                           GLIDE_IS_LAND(model%geometry%thkmask), &
+                           plume_imin,plume_imax,plume_kmin,plume_kmax)
+     
+     
+     call plume_io_initialize(plume_suppress_ascii_output, &
+                              trim(plume_output_nc_file))
 
-!  call plume_initialise(plume_nl,model%geometry%lsrf * thk0)
+ 
+     !run the plume to steady state with given shelf draft
+     call plume_iterate(time, &
+                        model%numerics%tinc*1.d0, &
+                        model%geometry%lsrf * thk0, &
+                        model%temper%temp(model%general%upn-1,:,:), &
+                        (model%numerics%sigma(model%general%upn) - model%numerics%sigma(model%general%upn -1)) * &
+                        model%geometry%thck * thk0, &
+                        model%temper%bmlt, &
+                        model%temper%temp(model%general%upn,:,:), &        
+                        plume_steadiness_tol, &
+                        plume_min_spinup_time, &
+                        run_plume_to_steady = .true.,&
+                        write_all_states = plume_write_all_states)
+                                               
 
- !run the plume to steady state with given shelf draft
- !call plume_iterate(time, &
- !                   model%numerics%tinc*1.d0, &
- !                   model%geometry%lsrf(:,:) * thk0, &
- !                   model%temper%temp(:,:,model%general%upn-1), &
- !                   (model%numerics%sigma(model%general%upn) - &
- !                   model%numerics%sigma(model%general%upn -1)) * &
-!	             model%geometry%thck(:,:), &
-!                    model%temper%bmlt(:,:), &
-!                    model%temper%temp(:,:,model%general%upn), &            
-!                    .true., &
-!                    plume_stopping_tol = 1.d-10)
+ end if
 
- write(*,*) model%temper%bmlt(10,:)
-
-
- do while(time.le.model%numerics%tend)
-
-     write(*,*)'beginning of timestep at time =', time
+ do while(time .le. model%numerics%tend)
 
      model%climate%acab(:,:) = climate_cfg%accumulation_rate
      model%climate%artm(:,:) = climate_cfg%artm
+     model%climate%eus = climate_cfg%eus
 
-     write(*,*)'ice step'
-     call glide_tstep_p1(model,time) ! temp evolution, basal traction
-     call glide_tstep_p2(model)      ! velocities, thickness advection, basal stress
+     call glide_tstep_p1(model,time) ! temp evolution
+     call glide_tstep_p2(model)      ! velocities, thickness advection
      call glide_tstep_p3(model)      ! isostasy, upper/lower surfaces
 
      time = time + model%numerics%tinc
      
-!     write(*,*)'plume_iterate'
-!     call plume_iterate(time, &
-!                       model%numerics%tinc*1.d0, &
-!                       model%geometry%lsrf(:,:) * thk0, &
-!                       model%temper%temp(:,:,model%general%upn-1), &
-!                      (model%numerics%sigma(model%general%upn) - &
-!                       model%numerics%sigma(model%general%upn -1)) * &
-!	               model%geometry%thck, &
-!                       model%temper%bmlt, &
-!                       model%temper%temp(:,:,model%general%upn), &                    
-!                       run_plume_to_steady = .false., &
-!                       plume_stopping_tol = 1.0d-10)
+     if (model%options%which_bmlt .eq. USE_PLUME) then	
+	 
+          call plume_iterate(time, &
+                    model%numerics%tinc*1.d0, &
+                    model%geometry%lsrf(:,:) * thk0, &
+                    model%temper%temp(model%general%upn-1,:,:), &
+                    (model%numerics%sigma(model%general%upn) - model%numerics%sigma(model%general%upn -1)) * &
+                    model%geometry%thck(:,:) * thk0, &
+                    model%temper%bmlt(:,:), &
+                    model%temper%temp(model%general%upn,:,:), &            
+	            plume_steadiness_tol, &
+                    plume_min_subcycle_time, &
+                    run_plume_to_steady = .false., &
+                    write_all_states = plume_write_all_states)
 
+     end if
   end do
 
   ! finalise GLIDE
   call glide_finalise(model)
- ! call plume_finalise()	
- ! call plume_io_finalize()
+
+    if (model%options%which_bmlt .eq. USE_PLUME) then 
+      call plume_finalise()	
+      call plume_io_finalize()
+      call plume_logging_finalize()
+    end if
+
   call system_clock(clock,clock_rate)
   t2 = real(clock,kind=dp)/real(clock_rate,kind=dp)
   call glimmer_writestats(commandline_resultsname,commandline_configname,t2-t1)
-  call close_log
+  call close_log()
 
 contains
 
-subroutine plume_read_print_config(model,config,plume_nl_file,plume_supp_output)
+  subroutine shelf_config_initialise(climate_cfg,config)
+    !*FD initialise shelf climate model
+    
+    use glimmer_paramets, only: thk0, acc0, scyr
+    use glimmer_config
+    use glimmer_log
+    
+implicit none
+
+    type(shelf_climate) :: climate_cfg         !*FD structure holding climate info
+    type(ConfigSection), pointer :: config  !*FD structure holding sections of configuration file   
+    
+    ! local variables
+    type(ConfigSection), pointer :: section
+    real(kind=sp), dimension(:), pointer :: dummy
+    character(len=100) :: message
+
+    call GetSection(config,section,'Petermann shelf')
+    if (associated(section)) then
+        call GetValue(section,'air_temperature',climate_cfg%artm)
+        call GetValue(section,'accumulation_rate',climate_cfg%accumulation_rate)   
+        call GetValue(section,'eustatic_sea_level',climate_cfg%eus)
+    else
+       !log error
+       call write_log('No Petermann section',GM_FATAL)
+    end if
+
+    call write_log_div
+    call write_log('Petermann shelf configuration')
+    call write_log('------------------------------------')
+    write(message,*) 'air temperature  : ',climate_cfg%artm
+    call write_log(message)
+    write(message,*) 'accumulation rate: ',climate_cfg%accumulation_rate
+    call write_log(message)
+    write(message,*) 'eustatic sea level:',climate_cfg%eus
+    call write_log(message)
+           
+ end subroutine shelf_config_initialise
+
+ subroutine plume_read_print_config(model,config, &
+                                    plume_nl_file, &
+                                    plume_output_dir, &
+                                    plume_output_prefix, &
+                                    plume_output_nc_file, &
+                                    plume_suppress_ascii_output,&
+                                    plume_suppress_logging)
 
     !*FD read configuration file for plume-related things
 
@@ -184,28 +237,53 @@ subroutine plume_read_print_config(model,config,plume_nl_file,plume_supp_output)
     type(glide_global_type) :: model       
     type(ConfigSection), pointer :: config 
                                            
-    character(len=*),intent(out) :: plume_nl_file
-    logical,intent(out) :: plume_supp_output
+    character(len=*),intent(out) :: plume_nl_file,plume_output_nc_file, &
+                                    plume_output_dir,plume_output_prefix
+    logical,intent(out) :: plume_suppress_ascii_output,plume_suppress_logging
 
     ! local variables
     type(ConfigSection), pointer :: section
     character(len=512) :: message
 
     call GetSection(config,section,'plume')
-    if (associated(section)) then
-        call GetValue(section, 'plume_nl_file', plume_nl_file)
-	call GetValue(section, 'suppress_output', plume_supp_output)	
-    else
+    if (.not. associated(section)) then
 	call write_log('for shelf driver there must be a [plume] &
                         section in config file',  &
 	          GM_FATAL)
-    endif 	          		
+    end if
+    call GetValue(section, 'plume_nl_file', plume_nl_file)
+    call GetValue(section, 'plume_output_dir', plume_output_dir)
+    call GetValue(section, 'plume_output_file', plume_output_nc_file)
+    call GetValue(section, 'suppress_ascii_output', plume_suppress_ascii_output)
+    call GetValue(section, 'suppress_logging', plume_suppress_logging)
+    call GetValue(section, 'plume_output_prefix',plume_output_prefix)
+    call GetValue(section, 'plume_write_all_states',plume_write_all_states)
+    call GetValue(section, 'plume_min_spinup_time',plume_min_spinup_time)
+    call GetValue(section, 'plume_min_subcycle_time',plume_min_subcycle_time)
+    call GetValue(section, 'plume_steadiness_tol', plume_steadiness_tol)
 
     call write_log('Plume config')
     write(message,*) 'plume namelist file:', trim(plume_nl_file)
     call write_log(message)
-    write(message,*) 'suppressing plume output:',plume_supp_output
- 	
+    write(message,*) 'suppressing plume output:',plume_suppress_ascii_output
+    call write_log(message)
+    write(message,*) 'suppressing plume screen/file logging:',plume_suppress_logging
+    call write_log(message)
+    write(message,*) 'plume output written to netcdf file:', trim(plume_output_nc_file)
+    call write_log(message)
+    write(message,*) 'plume old-style output written to directory:', trim(plume_output_dir)
+    call write_log(message)
+    write(message,*) 'plume output file prefix (jobid):', trim(plume_output_prefix)
+    call write_log(message)
+    write(message,*) 'plume_write_all_states:', plume_write_all_states
+    call write_log(message)
+    write(message,*) 'plume_min_spinup_time',plume_min_spinup_time
+    call write_log(message)
+    write(message,*) 'plume_min_subcycle_time', plume_min_subcycle_time
+    call write_log(message)
+
+	
 end subroutine
+
 
 end program shelf_driver
