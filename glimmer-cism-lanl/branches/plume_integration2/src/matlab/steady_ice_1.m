ce2d61f86ecd0b60577d05d3a1e702806f31096c
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-06-25 22:41:12 +0000

    little bug in matlab steady-ice

--- glimmer-cism-lanl/branches/plume_integration2/src/matlab/steady_ice_1.m
+++ glimmer-cism-lanl/branches/plume_integration2/src/matlab/steady_ice_1.m
@@ -8,7 +8,7 @@ if (a_net_per_year ~= 0.0)
       (A/a_net_per_year)*((1-rhoi/rhoo)*rhoi*g/4)^3 * ...
       ( wfun(x0)^4 - wfun(x).^4);
 
-    x = x0:(x1-x0)/n:x1;
+    x = x0:(x1-x0)/(n-1):x1;
     u = -1.0* ufun4(x) .^(1/4);
     h = wfun(x)./u;
     w = wfun(x);
@@ -16,7 +16,7 @@ if (a_net_per_year ~= 0.0)
 else
     wfun = @(x) u0*h0;
     ufun4 = @(x) u0^4 - (4*A)*((1-rhoi/rhoo)*rhoi*g/4)^3*(u0*h0)^3*(x0-x);
-    x = x0:(x1-x0)/n:x1;
+    x = x0:(x1-x0)/(n-1):x1;
     u = -1.0* ufun4(x) .^(1/4);
     h = u0*h0./u;
     w = wfun(x);
