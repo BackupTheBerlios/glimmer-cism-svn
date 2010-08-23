584a8a74a8b07b9d7588020d12de8aa918e7191e
sprice <sprice@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-08-23 21:57:20 +0000

    Minor changes to solver flags module so that code will build on Mac servers (changed name from solver_mod.F90 to solver_flags.F90, initial value assigned to flag NL_solver within this mode - no idea why this is necessary, but won't compile otherwise). JF, please confirm that this does not present a problem for the way you intended this flag to work.

---
+++ glimmer-cism-lanl/branches/parallel/src/fortran/solver_flags.F90
@@ -0,0 +1,8 @@
+
+module solver_flags
+
+    implicit none
+
+    integer :: NL_solver = 1
+
+end module solver_flags
--- glimmer-cism-lanl/branches/parallel/src/fortran/solver_mod.F90
+++
@@ -1,6 +0,0 @@
-MODULE solver_flags
-
-      IMPLICIT NONE
-      INTEGER :: NL_solver
-
-END MODULE solver_flags
