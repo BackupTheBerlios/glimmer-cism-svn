5eb3bb91e3a826e00a52ff9e3ae5ebb7ee49c071
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-09-14 03:10:44 +0000

    fixing the kickoff_steady script to use flow_law:2, and new tool to extract last time slice from each file in a glob of outputted netcdf files, also added a README file for the matlab scripts

---
+++ glimmer-cism-lanl/branches/plume_integration2/tests/petermann/nc_last_time_slice.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+
+import sys
+import subprocess
+import re
+import os
+
+USAGE = 'nc_last_time_slice.py <nc_file_in>*'
+
+
+def process_files(fnames):
+    
+    for fname in fnames:
+        #print('scanning %s' % fname)
+        p = subprocess.Popen(['ncdump','-h',fname],stdout=subprocess.PIPE)
+        (output,err) = p.communicate()
+        final_time = int(re.search(r'UNLIMITED ; // \(([0-9]+) currently',
+                                   output).group(1))
+
+        cmd = ['ncra','-dtime,%s,%s' % (final_time-1,
+                                                        final_time),
+                               fname,
+                               './last_time.%s' % os.path.basename(fname)]
+        #print(cmd)
+        retcode = subprocess.call(cmd)
+        if (retcode != 0):
+            raise Exception("%s failed" % " ".join(cmd))
+        
+        
+        
+
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+        print (USAGE)
+        
+    fnames = sys.argv[1:]
+
+    process_files(fnames)
+    
+
+
+
--- glimmer-cism-lanl/branches/plume_integration2/tests/petermann/task_scripts/kickoff_steady.py
+++ glimmer-cism-lanl/branches/plume_integration2/tests/petermann/task_scripts/kickoff_steady.py
@@ -1,58 +1,85 @@
 from submit_GC_job import submit_job
 from gcplume import *
+
 import os
+import sys
 
-for tau_xy_0 in [0,10, 25, 50]:
-    for acab in [-2.0, 0.0, 2.0]:
+def kickoff(unique_str, queue_mode, email, walltime):
 
-        j = SteadyShelfJob()
-        j.name = 'ssj1_%s_kPa_%s_acab' % (tau_xy_0, acab)
-        jd = os.path.expandvars('$GC_JOBS/%s' % j.name)
-        if (not(os.path.lexists(jd))):
-            os.mkdir(jd)
-        j.jobDir = jd
-
-        # varying properties
-        j.uniform_acab = acab
-
-        # fixed default properties
-        j.m = 20
-        j.n = 40
-        j.hx = 1000.0
-        j.hy = 1000.0
-        j.nlevel = 3
-
-        j.default_flwa = 1.0e-16
-        j.upvel = -1000.0
-        j.upthk = 600.0
-        j.randthk = 0.0
-        j.otopg = -2000.0
-
-        j.tend = 200.0
-        j.tstart = 0.0
-        j.ice_dt = 0.1
-
-        j.use_plume = 0
-        j.plume_dt = 0.0
-        
-        j.gc = {'options' : { 'flow_law' : 0,
-                              'temperature' : 0,
-                              },
-                'Petermann shelf' : { 'accumulation_rate' : acab,
-             		              'thk_steady_tol' : 1.0e-3*j.ice_dt,
-                                     },
-                'boundary condition params' : {'tau_xy_0' : tau_xy_0 * 1000.0,
-                                               },
-	        'picard parameters' : {'small_vel' : 0.01,
-	 			       'minres' : 1.0e-6,
-				       'y_overrideres' : 1.0e-9,
-					'cvg_accel' : 1.25,
-		                      },
-                                               
-                }
-        j.assertCanStage()
-        j.serialize()
-        print( "submitting job %s" % j.name)
-        submit_job(j,'gladish@cims.nyu.edu','24:00:00','q')
     
+    for tau_xy_0 in [0,10, 25, 50]:
+        for acab in [-2.0, 0.0, 2.0]:
+            
+            j = SteadyShelfJob()
+            j.name = 'ssj1_%s_kPa_%s_acab_%s' % (tau_xy_0, acab,unique_str)
+            jd = os.path.expandvars('$GC_JOBS/%s' % j.name)
+            if (not(os.path.lexists(jd))):
+                os.mkdir(jd)
+            j.jobDir = jd
+
+            # varying properties
+            j.uniform_acab = acab
+
+            # fixed default properties
+            j.m = 20
+            j.n = 40
+            j.hx = 1000.0
+            j.hy = 1000.0
+            j.nlevel = 3
+
+            j.default_flwa = 1.0e-16
+            j.upvel = -1000.0
+            j.upthk = 600.0
+            j.randthk = 0.0
+            j.otopg = -2000.0
+
+            j.tend = 400.0
+            j.tstart = 0.0
+            j.ice_dt = 0.1
+
+            j.use_plume = 0
+            
+            j.plume_dt = 0.0
+
+            j.plume = {'salttop' : 0.0,
+                       'saltbot' : 0.0,
+                       'temptop' : 0.0,
+                       'tempbot' : 0.0,
+                       'phi' : 0.0}
+            
+            j.gc = {'options' : { 'flow_law' : 2,
+                                  'temperature' : 0,
+                                  },
+                    'Petermann shelf' : { 'accumulation_rate' : acab,
+                                          'thk_steady_tol' : 1.0e-3*j.ice_dt,
+                                          },
+                    'boundary condition params' : {'tau_xy_0' : tau_xy_0 * 1000.0,
+                                                   },
+                    'picard parameters' : {'small_vel' : 0.01,
+                                           'minres' : 1.0e-6,
+                                           'y_overrideres' : 1.0e-9,
+                                           'cvg_accel' : 1.25,
+                                           },
+#                    'plume' : {'plume_const_bmlt' : True,
+#                               'plume_const_bmlt_rate' : -1.0 * acab,
+#                               },
+                    }
+            j.assertCanStage()
+            j.serialize()
+            print( "submitting job %s" % j.name)
+            submit_job(j,email,walltime,queue_mode)
+    
+    
+
+USAGE = 'python kickoff_steady.py <unique_str> <queue_mode>'
+
+if __name__ == '__main__':
+
+    if (len(sys.argv) != 3):
+        print (USAGE)
+
+    else:
+        kickoff( sys.argv[1], sys.argv[2],'gladish@cims.nyu.edu','24:00:00')
+    
+
     
