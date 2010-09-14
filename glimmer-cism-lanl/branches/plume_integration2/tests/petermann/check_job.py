6dff046bda5891263366b111a1d8ba523054de69
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-09-14 20:34:48 +0000

    new tool to check on jobs as they are running

---
+++ glimmer-cism-lanl/branches/plume_integration2/tests/petermann/check_job.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import gcplume
+import pickle
+import sys
+import time
+
+USAGE = "check_job.py [--printCompleted] <jobfile>"
+
+
+if (len(sys.argv) < 2 ):
+
+    raise Exception('Not enough arguments.\n Usage:\n %s \n' % USAGE)
+
+if (sys.argv[1] == '--printCompleted'):
+    printCompleted = True
+    jfiles = sys.argv[2:]
+else:
+    printCompleted = False
+    jfiles = sys.argv[1:]
+
+for jfile in sys.argv[2:]:
+
+    f = open(jfile,'r')
+
+    try:
+        j = pickle.load(f)
+    finally:
+        f.close()
+
+    msg = ''
+
+    printMsg = True
+
+    try:
+        
+        if (not(j.started)):
+            msg = "not started"
+        elif (j.started and not(j.completed or j.error)):
+            msg = "running"
+
+        elif (j.completed):
+            msg = "completed"
+            if (not(printCompleted)):
+                printMsg = False
+        elif (j.error):
+            msg = "error: %s" % j.errorMessage
+
+    except AttributeError:
+        msg = "old job version"
+        
+    if (printMsg):
+        print ("%s: %s" % (j.name, msg))
+            
