df842d2a97220d3f4e3f5b221100b657d86f6747
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-09-15 23:25:26 +0000

    updates to matlab scripts, and README file

--- glimmer-cism-lanl/branches/plume_integration2/src/matlab/plot_1d_flat_shelf_model_and_exact.m
+++ glimmer-cism-lanl/branches/plume_integration2/src/matlab/plot_1d_flat_shelf_model_and_exact.m
@@ -1,54 +1,60 @@
 %% Plot flat shelf profiles
 
-f1 = '/scratch/gc_output/l9/sunstudio.out.nc';
-%f2 = '/scratch/gc_output/l6/sunstudio.out.nc';
+f1 = '/home/gladish/research/gcp_resources/nc_files/last_time.flat_shelf_100_m_sept15.2.out.nc';
+f2 = '/home/gladish/research/gcp_resources/nc_files/last_time.flat_shelf_200_m_sept15.2.out.nc';
+f3 = '/home/gladish/research/gcp_resources/nc_files/last_time.flat_shelf_500_m_sept15.2.out.nc';
 
-hx = 1000;
-hy = hx;
 m = 5;
-n = 41;
+n = 100;
+
+hy = 40000.0 / n;
+hx = hy;
+
 kinbcw = 2;
 x0 = (n-kinbcw-0.5)*hy;
-x1 = (4-0.5)*hy;
+x1 = (4.0)*hy;
 A = 1.0*10^(-16);   % Pa^(-3)* a^(-1)
 n_points = 1000;
-u0 = -1000;
-h0 = 1000;
+
+u0 = -1000.0;
+
 rhoi = 910.0;
 rhoo = 1028.0;
 g = 9.81;
 
 fs = 18;
 
+hold on
+
 [~,y0,~,y1,thck,~,vvel] = nc_read(f1, -1);
 [ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
+h0 = 100.0;
 [y,v] = flat_shelf_ice(A,rhoi,rhoo,g,x0,x1,n_points, u0, h0);
-
-%subplot(2,1,1);
-hold on
 plot(ycvel/1000, vvelc, 'b*');
-plot(y/1000, v, 'r');
-title('Velocity','FontSize',fs);
+plot(y/1000, v, 'k');
+
+[~,y0,~,y1,thck,~,vvel] = nc_read(f2, -1);
+[ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
+h0 = 200.0;
+[y,v] = flat_shelf_ice(A,rhoi,rhoo,g,x0,x1,n_points, u0, h0);
+plot(ycvel/1000, vvelc, 'r*');
+plot(y/1000, v, 'k');
+
+[~,y0,~,y1,thck,~,vvel] = nc_read(f3, -1);
+[ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
+h0 = 500.0;
+[y,v] = flat_shelf_ice(A,rhoi,rhoo,g,x0,x1,n_points, u0, h0);
+plot(ycvel/1000, vvelc, 'g*');
+plot(y/1000, v, 'k');
+
+title('Diagnosed Velocity - flat shelf','FontSize',fs);
 ylabel('m/year','FontSize',fs);
 xlabel('km','FontSize',fs);
 
-
-%[~,y0,~,y1,thck,~,vvel] = nc_read(f2, -1);
-%[ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
-%plot(ycvel/1000, vvelc, 'r*');
-
-%subplot(2,1,2);
-%hold on
-%plot(ycthk/1000, thkc, 'b*');
-%plot(y/1000, h, 'b');
-%title('Thickness','FontSize',fs);
-%ylabel('meters','FontSize',fs);
-%xlabel('km','FontSize',fs);
-
-legend('computed v vel (m/year)', 'exact sol. v vel (m/year)', ...
+legend( 'model vel (100 m thk)','exact vel (100 m thk)', ...
+        'model vel (200 m thk)','exact vel (200 m thk)', ...
+        'model vel (500 m thk)','exact vel (500 m thk)', ...
        'Location','BestOutside');    
-%       '10.0 m/year','10.0 m/year Exact Sol.', ...
-%       '25.0 m/year','25.0 m/year Exact Sol.', ...
 
 hold off
 
--- glimmer-cism-lanl/branches/plume_integration2/src/matlab/plot_1d_steady_states_model_and_exact.m
+++ glimmer-cism-lanl/branches/plume_integration2/src/matlab/plot_1d_steady_states_model_and_exact.m
@@ -1,20 +1,26 @@
 %% Plot steady profiles
 
-f0 = '~/research/gcp_resources/nc_files/1d_0_bmlt_sept12.5.out.nc';
-f1 = '~/research/gcp_resources/nc_files/1d_10_bmlt_sept12.5.out.nc';
-f2 = '~/research/gcp_resources/nc_files/1d_25_bmlt_sept12.5.out.nc';
+f0 = '~/research/gcp_resources/nc_files/1d_0_bmlt_sept15.1.out.nc';
+f1 = '~/research/gcp_resources/nc_files/1d_10_bmlt_sept15.1.out.nc';
+f2 = '~/research/gcp_resources/nc_files/1d_25_bmlt_sept15.1.out.nc';
 
-hx = 500.0;
-hy = hx;
 m = 5;
-n = 81;
+n = 100;
+
+hx = 40000.0 / n;
+hy = hx;
+
 kinbcw = 2;
 ythk0 = (n-kinbcw)*hy;
+yvel0 = (n-kinbcw-0.5)*hy;
 yend = 4*hy;
+
 A = 1.0*10^(-16); 
+
 n_points = 1000;
 u0 = -1000;
 h0 = 1000;
+
 rhoi = 910.0;
 rhoo = 1028.0;
 g = 9.81;
@@ -28,8 +34,8 @@ plot5 = false;
 
 [~,y0,~,y1,thck,~,vvel] = nc_read(f0, -1);
 [ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
-bmelt = 0.0;
-[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, bmelt);
+acab = 0.0;
+[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, acab);
 
 subplot(2,1,1);
 hold on
@@ -50,8 +56,8 @@ xlabel('km','FontSize',fs);
 if (plot1) 
 [~,y0,~,y1,thck,~,vvel] = nc_read(f1, -1);
 [ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
-bmelt = -10.0;
-[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, bmelt);
+acab = -10.0;
+[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, acab);
 
 subplot(2,1,1);
 plot(ycvel/1000, vvelc, 'k*');
@@ -65,8 +71,8 @@ end
 if (plot2) 
 [~,y0,~,y1,thck,~,vvel] = nc_read(f2, -1);
 [ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
-bmelt = -25.0;
-[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, bmelt);
+acab = -25.0;
+[y,v,h,~] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, acab);
 
 subplot(2,1,1);
 plot(ycvel/1000, vvelc, 'r*');
@@ -80,8 +86,8 @@ end
 if (plot5)
 [~,y0,~,y1,thck,~,vvel] = nc_read(f5, -1);
 [ycvel,ycthk, vvelc, thkc] = centerline_profile(y0,y1,vvel,thck);
-bmelt = -5.0;
-[y,v,h,w] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, bmelt);
+acab = -5.0;
+[y,v,h,w] = steady_ice_1(A,rhoi,rhoo,g,ythk0,yend,n_points, u0, h0, acab);
 
 subplot(2,1,1);
 plot(ycvel/1000, vvelc, 'g*');
