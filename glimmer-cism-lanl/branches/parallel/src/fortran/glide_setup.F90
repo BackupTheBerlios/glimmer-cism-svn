67ef2898530a5bdf9d8c72c0b84896550af7d090
mbougamont <mbougamont@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-09-15 13:19:53 +0000

    Minor changes in glide_setup.F90: adjusted the size of character arrays in subroutine *print_options*, to prevent a compiling error with gfortran (e.g., v4.5)

--- glimmer-cism-lanl/branches/parallel/src/fortran/glide_setup.F90
+++ glimmer-cism-lanl/branches/parallel/src/fortran/glide_setup.F90
@@ -455,31 +455,31 @@ contains
     ! local variables
     character(len=*), dimension(0:2), parameter :: temperature = (/ &
          'isothermal', &
-         'full',       &
-         'steady'      /)
+         'full      ', &
+         'steady    ' /)
     character(len=*), dimension(0:2), parameter :: flow_law = (/ &
-         'Paterson and Budd               ', &
-         'Paterson and Budd (temp=-10degC)', &
+         'Paterson and Budd                ', &
+         'Paterson and Budd (temp=-10degC) ', &
          'const 1e-16a^-1Pa^-n             '/)
   !*mb* added options for basal processes model       
     character(len=*), dimension(0:2), parameter :: which_bmod = (/ &
          'Basal proc mod disabled '  , &
-         'Basal proc, high res. '   , &
-         'Basal proc, fast calc.' /)
+         'Basal proc, high res.   '   , &
+         'Basal proc, fast calc.  ' /)
     character(len=*), dimension(0:4), parameter :: basal_water = (/ &
-         'local water balance', &
-         'local + const flux ', &
-         'none               ', &
-         'From basal proc model  ', &
-         'Constant value (=10m)'/)
+         'local water balance   ', &
+         'local + const flux    ', &
+         'none                  ', &
+         'From basal proc model ', &
+         'Constant value (=10m) '/)
     character(len=*), dimension(0:7), parameter :: marine_margin = (/ &
-         'ignore            ', &
-         'no ice shelf      ', &
-         'threshold         ', &
-         'const calving rate', &
-         'edge threshold    ', &
-         'van der Veen      ', &
-         'Pattyn Grnd Line  ', &
+         'ignore              ', &
+         'no ice shelf        ', &
+         'threshold           ', &
+         'const calving rate  ', &
+         'edge threshold      ', &
+         'van der Veen        ', &
+         'Pattyn Grnd Line    ', &
          'Huybrechts Greenland'/)
     character(len=*), dimension(0:5), parameter :: slip_coeff = (/ &
          'zero        ', &
@@ -524,7 +524,7 @@ contains
          'no slip (using large B^2)              ', &
          'B^2 passed from CISM                   ', &
          'no slip (Dirichlet implementation)     ', &
-         'till yield stress (Newton) ' /)
+         'till yield stress (Newton)             ' /)
     character(len=*), dimension(0:1), parameter :: ho_whichefvs = (/ &
          'from eff strain rate    ', &
          'constant value          ' /)
@@ -538,19 +538,19 @@ contains
          'shelf front disabled    '/)
     !*sfp* added the next two for HO temperature calcs
     character(len=*), dimension(0:2), parameter :: dispwhich = (/ &
-         '0-order SIA               ', &
+         '0-order SIA                       ', &
          '1-st order model (Blatter-Pattyn) ', &
-         '1-st order depth-integrated (SSA)' /)
+         '1-st order depth-integrated (SSA) ' /)
     character(len=*), dimension(0:2), parameter :: bmeltwhich = (/ &
-         '0-order SIA               ', &
+         '0-order SIA                       ', &
          '1-st order model (Blatter-Pattyn) ', &
-         '1-st order depth-integrated (SSA)' /)
+         '1-st order depth-integrated (SSA) ' /)
     character(len=*), dimension(0:4), parameter :: ho_whichsparse = (/ &
-         'BiCG with LU precondition  ', &
-         'GMRES with LU precondition ', &
-         'Unsymmetric Multifrontal   ', &
-         'Compatible Trilinos interface ', &
-         'Standalone Trilinos interface '/)
+         'BiCG with LU precondition      ', &
+         'GMRES with LU precondition     ', &
+         'Unsymmetric Multifrontal       ', &
+         'Compatible Trilinos interface  ', &
+         'Standalone Trilinos interface  '/)
 
     call write_log('GLIDE options')
     call write_log('-------------')
