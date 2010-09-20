8c3f6c6bb2be8648d2ea776a04ea2e96526a5292
sprice <sprice@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2010-09-20 15:35:04 +0000

    Minor changes/improvements to matlab script for setting up and comparing model output to analytical ice stream solutions.

--- glimmer-cism-lanl/branches/parallel/tests/stream/stream.m
+++ glimmer-cism-lanl/branches/parallel/tests/stream/stream.m
@@ -9,12 +9,19 @@ cd /Users/sprice/work/modeling/cism-parallel/tests/stream/
 rho = 910;
 g = -9.81;
 
+flag = 0;       %% USE RAYMOND PROFILE
+flag = 1;       %% USE SCHOOF PROFILE
+
+kinflag = 1;    %% apply kinematic bc (analytic soln) at up/downstream ends
+% kinflag = 0;    %% apply 0 vel bc at up/downstream ends
+
 n = 3;
 m = 2;
 
 r = 20;         % !! r needs to be even # divisible by 2 !!
-c = 30;
-levels = 5;
+c = 20;
+% c = 200;
+levels = 11;
 
 L = 1.5e4; 
 A = 1e-16;
@@ -44,7 +51,6 @@ tau0r = 5e3;
 % Raymond solution
 ur = 2 * A / (n+1) * ( (taud - tau0r)/H )^n * ( W^(n+1) - yy.^(n+1) );
 
-
 us = -2*taud^3*L^4/(B^3*H^3) * ( ((yy/L).^4 - (m+1)^(4/m))/4 - 3*( abs(yy/L).^(m+4) ...
     - (m+1)^(1+4/m) )/((m+1)*(m+4)) + 3*( abs(yy/L).^(2*m+4) - (m-1)^(2+4/m) )/((m+1)^2*(2*m+4)) ...
     - ( abs(yy/L).^(3*m+4) - (m+1)^(3+4/m) )/ ( (m+1)^3*(3*m+4)) );
@@ -55,22 +61,27 @@ ind = find( abs( yy ) >= W ); us(ind) = min( min( us ) );
 
 us = us - min( us );
 
-
-figure(198), clf
-subplot(2,1,1), hold on
-plot( yy/1e3, ur - min(ur), 'k-', 'linewidth', 1.0 ), hold on, grid on
-xlabel( 'dist across flow (m)'), ylabel( 'velocity (m/a)'), title( 'Raymond solution' )
-subplot(2,1,2), hold on
-plot( yy/1e3, tau0r*ones(size(yy))/1e3, 'k-', 'linewidth', 1.0 ), hold on, grid on
-xlabel( 'dist across flow (m)'), ylabel( 'yield stress (kPa)')
-
-figure(199), clf
-subplot(2,1,1), hold on
-plot( yy/1e3, us - min(us), 'k-', 'linewidth', 1.0 ), hold on, grid on
-xlabel( 'dist across flow (m)'), ylabel( 'velocity (m/a)'), title( 'Schoof solution' )
-subplot(2,1,2), hold on
-plot( yy/1e3, tau0s/1e3, 'k-', 'linewidth', 1.0 ), hold on, grid on
-xlabel( 'dist across flow (m)'), ylabel( 'yield stress (kPa)')
+if( flag == 0 )
+    figure(198), clf
+    subplot(2,1,1), hold on
+    plot( yy/1e3, ur - min(ur), 'r-', 'linewidth', 2.0 ), hold on, grid on
+    xlabel( 'dist across flow (m)'), ylabel( 'velocity (m/a)'), title( 'Raymond solution' )
+    box on
+    subplot(2,1,2), hold on
+    plot( yy/1e3, tau0r*ones(size(yy))/1e3, 'r-', 'linewidth', 2.0 ), hold on, grid on
+    xlabel( 'dist across flow (m)'), ylabel( 'yield stress (kPa)')
+    box on
+else
+    figure(199), clf
+    subplot(2,1,1), hold on
+    plot( yy/1e3, us - min(us), 'r-', 'linewidth', 2.0 ), hold on, grid on
+    xlabel( 'dist across flow (m)'), ylabel( 'velocity (m/a)'), title( 'Schoof solution' )
+    box on
+    subplot(2,1,2), hold on
+    plot( yy/1e3, tau0s/1e3, 'r-', 'linewidth', 2.0 ), hold on, grid on
+    xlabel( 'dist across flow (m)'), ylabel( 'yield stress (kPa)')
+    box on
+end
 
 
 %% put data into .mat file, to be read by .py script w/ config file, to make input .nc files
@@ -79,15 +90,20 @@ topg = repmat( 5e3 + dsdx*dx*[0:c-1], r, 1 );
 usrf = topg + thck;
 
 
-tauf_profile = [ fliplr(tau0s(2:end)), tau0s ]';        % assign Schoof profile
-tauf = 1e6 * ones( r-1, c-1 );
-tauf(3:end-2,3:end-2) = repmat( tauf_profile, 1, c-5 );     %% no slip at/near up/downstream ends
-% tauf(3:end-2,:) = repmat( tauf_profile, 1, c-1 );     %% use periodic bcs for cont. along flow
-
-% tauf_profile = tau0r*ones(r-7,1);        % assign Raymond profile
-% tauf = 1e10 * ones( r-1, c-1 );
-% % tauf(4:end-3,3:end-2) = repmat( tauf_profile, 1, c-5 );     %% no slip at/near up/downstream ends
-% tauf(4:end-3,:) = repmat( tauf_profile, 1, c-1 );     %% use periodic bcs for cont. along flow
+if( flag == 0 )         % assign Raymond profile
+    tauf_profile = tau0r*ones(r-7,1);        
+    tauf = 1e5 * ones( r-1, c-1 );
+    % tauf(4:end-3,3:end-2) = repmat( tauf_profile, 1, c-5 );     %% no slip at/near up/downstream ends
+    tauf(4:end-3,:) = repmat( tauf_profile, 1, c-1 );     %% use periodic bcs for cont. along flow
+    u_profile = repmat( [ 0 0 fliplr(ur) ur(2:end) 0 0 ], levels, 1 );     
+else                    % assign Schoof
+    
+    tauf_profile = [ fliplr(tau0s(2:end)), tau0s ]';        
+    tauf = 1e5 * ones( r-1, c-1 );
+    % tauf(3:end-2,3:end-2) = repmat( tauf_profile, 1, c-5 );     %% no slip at/near up/downstream ends
+    tauf(3:end-2,:) = repmat( tauf_profile, 1, c-1 );     %% use periodic bcs for cont. along flow
+    u_profile = repmat( [ 0 0 fliplr(us) us(2:end) 0 0 ], levels, 1 );
+end
 
 
 % figure(200),clf
@@ -102,31 +118,32 @@ tauf(3:end-2,3:end-2) = repmat( tauf_profile, 1, c-5 );     %% no slip at/near u
 
 
 
-% %% optional: add analytic solution at up/downstream ends as kin vel bc
+%% optional: add analytic solution at up/downstream ends as kin vel bc
 kinbcmask = zeros(size(tauf));
 uvelhom = zeros( levels, r-1, c-1 );
 vvelhom = zeros( levels, r-1, c-1 );
 
-% comment these out for now kin bc
-ur_profile = repmat( [ 0 0 fliplr(ur) ur(2:end) 0 0 ], levels, 1 );     
-us_profile = repmat( [ 0 0 fliplr(us) us(2:end) 0 0 ], levels, 1 );
-% uvelhom( :, :, end ) = ur_profile; uvelhom( :, :, 1 ) = ur_profile; 
-uvelhom( :, :, end ) = us_profile; uvelhom( :, :, 1 ) = us_profile; 
-kinbcmask(:,1) = 1; kinbcmask(:,end) = 1;
-ind = find( tauf <= 0 ); tauf(ind) = 1e-10;
+if( kinflag == 1)
+    uvelhom( :, :, end ) = u_profile; uvelhom( :, :, 1 ) = u_profile; 
+    kinbcmask(:,1) = 1; kinbcmask(:,end) = 1;
+    ind = find( tauf <= 0 ); tauf(ind) = 1e-10;
+end
 
-
-
+    
 save stream.mat usrf topg thck tauf uvelhom vvelhom kinbcmask levels 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% plot output
-
+%% plot output
 
 %% open file
-filename = 'stream.out.nc'; 
+if( flag == 0 )
+    filename = 'stream.raymond.nc'; 
+else
+    filename = 'stream.schoof.nc'; 
+end
+
 ncid = netcdf.open( filename, 'nowrite' );
 
 %% get id for variable names (use ncview to find var names?)
@@ -143,20 +160,23 @@ btracty = netcdf.getvar(ncid, id_btracty )';
 yy2 = [ yy yy(end)+dx yy(end)+2*dx ];
 yy2 = [ -fliplr(yy2(2:end)), yy2 ];
 
-figure(198)
-subplot(2,1,1), hold on
-plot( yy2/1e3, uvel(:,round(c/2),1), 'bo:' )
-plot( yy2/1e3, uvel(:,end,1), 'b*' )              %% boundary value
-legend( 'analytic', 'model', 'model boundary' )
-subplot(2,1,2), hold on
-plot( yy2/1e3, -btractx(:,round(c/2),1)/1e3, 'bo:' )
+if( flag == 0 )
+    figure(198)
+    subplot(2,1,1), hold on
+    plot( yy2/1e3, uvel(:,round(c/2),1), 'bo:' )
+    plot( yy2/1e3, uvel(:,end,1), 'b*' )              %% boundary value
+    legend( 'analytic', 'model', 'model boundary' )
+    subplot(2,1,2), hold on
+    plot( yy2/1e3, -btractx(:,round(c/2),1)/1e3, 'bo:' )
+else
+    figure(199)
+    subplot(2,1,1), hold on
+    plot( yy2/1e3, uvel(:,round(c/2),1), 'bo:' )
+    plot( yy2/1e3, uvel(:,end,1), 'b*' )              %% boundary value
+    legend( 'analytic', 'model', 'model boundary' )
+    subplot(2,1,2), hold on
+    plot( yy2/1e3, -btractx(:,round(c/2),1)/1e3, 'bo:' )
+end
 
 
-figure(199)
-subplot(2,1,1), hold on
-plot( yy2/1e3, uvel(:,round(c/2),1), 'bo:' )
-plot( yy2/1e3, uvel(:,end,1), 'b*' )              %% boundary value
-legend( 'analytic', 'model', 'model boundary' )
-subplot(2,1,2), hold on
-plot( yy2/1e3, -btractx(:,round(c/2),1)/1e3, 'bo:' )
 
