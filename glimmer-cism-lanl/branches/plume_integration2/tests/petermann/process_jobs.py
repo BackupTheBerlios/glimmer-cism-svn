3ffab3fe1f99bef2c4f7d657d50b0602ed3a8abc
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2011-05-29 20:12:23 +0000

    unifying the process_jobs scripts

--- glimmer-cism-lanl/branches/plume_integration2/tests/petermann/process_jobs.py
+++ glimmer-cism-lanl/branches/plume_integration2/tests/petermann/process_jobs.py
@@ -8,78 +8,87 @@ import subprocess
 import sys
 from submit_GC_job import *
 
-def main(jobsPrefix,doRestart,pattern=None):
+def main(jobsPrefix):
     
-    UNEXPLAINED = 0
-    PLUME_ERROR = 1
-    ICE_ERROR = 2
-    STEADY_STATE = 3
-    TIMED_OUT = 4
-    RESTART = 5
-
     jobs_dir  = os.path.join(os.path.expandvars('$GC_JOBS'))
     all_jobs = os.path.os.listdir(jobs_dir)
     jobs = [jdir for jdir in all_jobs if jdir.startswith(jobsPrefix)]
 
+    job_list = []
+    
     steady_ice_list = []
     plume_error_list = []
     ice_error_list = []
+    job_error_list = []
+
+    not_started_list = []
+    still_running_list = []
     killed_list = []
+    
     unexplained_list = []
-    restart_list = []
-    restart_jobs = []
-    job_list = []
 
-    #pattern = 'apr22.2_k_([0-9\.]*)_amp_([0-9\.]*)_upvel_([0-9\.]*)_temp_([\-0-9\.]*)_itemp_([\-0-9\.]*)_tvel_([0-9\.]*)'
-    #pattern = 'may_([0-9\.]*)_amp_([0-9\.]*)_upvel_([0-9\.]*)_temp_([\-0-9\.]*)_itemp_([\-0-9\.]*)_tvel_([0-9\.]*)'
-    pattern = pattern or 'may21.T([profile\-0-9\.]*)_coupled_temp_([\-0-9\.]*)_tau_([0-9\.]*)_diff_([0-9\.]*)_k_([0-9\.]*)_amp_([0-9]*\.[0-9]*)'
-
+    kv_pattern = '_([a-z,A-Z]+)_([-\.0-9]+)'
+    
     for jname in jobs:
+        
         unexplained = True
-        g = re.match(pattern,jname).group
+        stillRunning = True
+        
+        kv_list = re.findall(kv_pattern, jname[len(jobsPrefix):len(jname)])
+        job_key = tuple([v for (k,v) in kv_list])
+        print(jname)
+        
+        job_list.append(jname)
 
-        job_key = (str(g(1)),float(g(2)),float(g(3)),
-                   float(g(4)),float(g(5)),float(g(6)))
+        #try:
+        #    job_file = open(os.path.join(os.path.expandvars('$GC_JOBS'),
+        #                                 jname,'%s.gcpl' % jname),'r')
+        #    j_obj = pickle.load(job_file)
+        #    job_file.close()
+        #except:
+        #    print('Could not load job_file %s' % job_file)
+        #    return
 
-        print(job_key)
-        job_list.append(job_key)
 
-        job_file = open(os.path.join(os.path.expandvars('$GC_JOBS'),
-                                     jname,'%s.gcpl' % jname),'r')
-        try:
-            j_obj = pickle.load(job_file)
-        finally:
-            job_file.close()
-
-        # take a look at the ice log file
-        log_file = os.path.join(os.path.expandvars('$GC_JOBS'),
-                                jname,'%s.config.log' % jname)
-
-        cmd = ['tail','-n20',log_file]
-        log_tail_p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-        log_tail_p.wait()
-        final_log_lines = log_tail_p.stdout.readlines()
-
-        for l in final_log_lines:
-            if l.strip().startswith('Stopped time-stepping'):
-                unexplained = False
-                steady_ice_list.append(job_key)
-
-            if ('ERROR' in l):
-                unexplained = False
-                ice_error_list.append(job_key)
-                restart_jobs.append(j_obj)
-
+        # list the files in the job directory
         job_contents_p = subprocess.Popen(['ls',os.path.join(os.path.expandvars('$GC_JOBS'),jname)],
                                           stdout=subprocess.PIPE)
         job_contents_p.wait()
         job_contents = job_contents_p.stdout.readlines()
+
+        job_log_file = ''
         plume_output_file = ''
+        ice_log_file = ''
+        
         for job_file in job_contents:
-            if job_file.strip().endswith('_output'):
-                plume_output_file = os.path.join(os.path.expandvars('$GC_JOBS'),
-                                                 jname,
-                                                 job_file.strip())
+            if job_file.strip().endswith('.out.log'):
+                job_log_file = os.path.join(os.path.expandvars('$GC_JOBS'),jname,job_file.strip())
+            elif job_file.strip().endswith('_output'):
+                plume_output_file = os.path.join(os.path.expandvars('$GC_JOBS'),jname,job_file.strip())
+            elif job_file.strip().endswith('.config.log'):
+                ice_log_file = os.path.join(os.path.expandvars('$GC_JOBS'),jname,job_file.strip())
+
+        # take a look at the ice log file
+        if (ice_log_file):
+            cmd = ['tail','-n20',ice_log_file]
+            log_tail_p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+            log_tail_p.wait()
+            final_log_lines = log_tail_p.stdout.readlines()
+
+            for l in final_log_lines:
+                if l.strip().startswith('Stopped time-stepping'):
+                    unexplained = False
+                    stillRunning = False
+                    steady_ice_list.append(jname)
+
+                if ('ERROR' in l):
+                    unexplained = False
+                    stillRunning = False
+                    ice_error_list.append(jname)
+        else:
+            not_started_list.append(jname)
+            unexplained = False
+            stillRunning = False
 
         if plume_output_file:
             cmd = ['tail','-n100',plume_output_file]
@@ -90,102 +99,75 @@ def main(jobsPrefix,doRestart,pattern=None):
             for l in plume_output_final_lines:
                 if 'error' in l:
                     unexplained = False
-                    plume_error_list.append(job_key)
-                    restart_jobs.append(j_obj)
+                    stillRunning = False
+                    plume_error_list.append(jname)
+
         else:
             print('Warning: did not find _output file for %s' % jname)
 
         # take a look at the job output log file to see if job was killed
-        outlog_file = os.path.join(os.path.expandvars('$GC_JOBS'),
-                                   jname,'%s.out.log' % jname)
+        if (job_log_file):
 
-        cmd = ['tail','-n100',outlog_file]
-        outlog_tail_p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
-        outlog_tail_p.wait()
-        final_outlog_lines = outlog_tail_p.stdout.readlines()
+            cmd = ['tail','-n100',job_log_file]
+            outlog_tail_p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+            outlog_tail_p.wait()
+            final_outlog_lines = outlog_tail_p.stdout.readlines()
 
-        for l in final_outlog_lines:
-            if 'process killed (SIGTERM)' in l:
-                unexplained = False
-                killed_list.append(job_key)
-                restart_jobs.append(j_obj)
-                break
-
-            if ('Error running:' in l and doRestart):
-                if(unexplained):
+            for l in final_outlog_lines:
+                if 'process killed (SIGTERM)' in l:
                     unexplained = False
-                    restart_list.append(job_key)
-                    job_gcpl_file = os.path.join(os.path.expandvars('$GC_JOBS'),
-                                                 jname,'%s.gcpl' % jname)
-                    restart_jobs.append(j_obj)
+                    stillRunning = False
+                    killed_list.append(jname)
+                    break
+
+                if ('Error running:' in l):
+                    stillRunning = False
+                    if not(jname in ice_error_list):
+                        unexplained = True
                     break
                 
-        if unexplained:
-            unexplained_list.append(job_key)
+        if (unexplained):
+            unexplained_list.append(jname)
 
-    print('%s in all jobs' % len(jobs))        
+        if (stillRunning):
+            still_running_list.append(jname)
+
+    print('%s in all jobs' % len(jobs))
+    
     print('%s in steady_ice_list' % len(steady_ice_list))
     print('%s in ice_error_list' % len(ice_error_list))
     print('%s in plume_error_list' % len(plume_error_list))
     print('%s in killed_list' % len(killed_list))
+    print('%s in still_running_list' % len(still_running_list))
+    print('%s in not_started_list' % len(not_started_list))
     print('%s in unexplained_list' % len(unexplained_list))
-    print('%s in restart_list' % len(restart_list))
     
-    print('intersections')
-    print(len(set(steady_ice_list).intersection(set(ice_error_list))))
-    print(len(set(steady_ice_list).intersection(set(plume_error_list))))
-    print(len(set(steady_ice_list).intersection(set(unexplained_list))))
-    print(len(set(steady_ice_list).intersection(set(killed_list))))
-    print(len(set(ice_error_list).intersection(set(plume_error_list))))
-    print(len(set(ice_error_list).intersection(set(unexplained_list))))
-    print(len(set(ice_error_list).intersection(set(killed_list))))
-    print(len(set(plume_error_list).intersection(set(unexplained_list))))
-    print(len(set(plume_error_list).intersection(set(killed_list))))
-    print(len(set(killed_list).intersection(set(unexplained_list))))
-
-    print('in restart and killed %s' % len(set(restart_list).intersection(set(killed_list))))
-    print('in restart and steady %s' % len(set(restart_list).intersection(set(steady_ice_list))))
-    print(len(set(restart_list).intersection(set(ice_error_list))))
-    print(len(set(restart_list).intersection(set(plume_error_list))))
-    print(len(set(restart_list).intersection(set(unexplained_list))))
-
     steady = open('jobs_%s_steady.txt' % (jobsPrefix),'w')
     iceerror=open('jobs_%s_iceerror.txt' % (jobsPrefix),'w')
     perror = open('jobs_%s_perror.txt' % (jobsPrefix),'w')
     killed = open('jobs_%s_killed.txt' % (jobsPrefix),'w')
+    stillrunning = open('jobs_%s_running.txt' % (jobsPrefix),'w')
+    notstarted = open('jobs_%s_notstarted.txt' % (jobsPrefix),'w')
     unex = open('jobs_%s_unexplained.txt' % (jobsPrefix),'w')
-    restart = open('jobs_%s_restart.txt' % (jobsPrefix),'w')
     
     try:
-        for job_key in job_list:
-            if job_key in steady_ice_list:
-                steady.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                             (job_key[0],job_key[1],job_key[2],job_key[3],
-                              job_key[4],job_key[5], STEADY_STATE))
-            elif job_key in plume_error_list:
-                perror.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                             (job_key[0],job_key[1],job_key[2],job_key[3],
-                              job_key[4],job_key[5], PLUME_ERROR))
-            elif job_key in ice_error_list:
-                iceerror.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                               (job_key[0],job_key[1],job_key[2],job_key[3],
-                                job_key[4],job_key[5], ICE_ERROR))
-            elif job_key in killed_list:
-                killed.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                             (job_key[0],job_key[1],job_key[2],job_key[3],
-                              job_key[4],job_key[5], TIMED_OUT))
-            elif job_key in unexplained_list:
-                unex.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                           (job_key[0],job_key[1],job_key[2],job_key[3],
-                            job_key[4],job_key[5], UNEXPLAINED))
-            elif job_key in restart_list:
-                restart.write('%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                           (job_key[0],job_key[1],job_key[2],job_key[3],
-                            job_key[4],job_key[5], RESTART))
+        for jname in job_list:
+            if jname in steady_ice_list:
+                steady.write('%s\n' % jname)
+            elif jname in plume_error_list:
+                perror.write('%s\n' % jname)
+            elif jname in ice_error_list:
+                iceerror.write('%s\n' % jname)
+            elif jname in killed_list:
+                killed.write('%s\n' % jname)
+            elif jname in unexplained_list:
+                unex.write('%s\n' % jname)
+            elif jname in still_running_list:
+                stillrunning.write('%s\n' % jname)
+            elif jname in not_started_list:
+                notstarted.write('%s\n' % jname)
             else:
-                print('not in any list:%s,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f,%8.1f\n' %
-                      (job_key[0],job_key[1],job_key[2],job_key[3],
-                       job_key[4],job_key[5], -1))
+                print('not in any list:%s' % jname)
 
     finally:
         steady.close()
@@ -194,19 +176,9 @@ def main(jobsPrefix,doRestart,pattern=None):
         killed.close()
         unex.close()
 
-    if (doRestart):
-        #for job_gcpl_file in restart_jobs:
-        for job_obj in restart_jobs:
-            #print('restarting job: %s' % job_gcpl_file)
-            #print('restarting %s' % str(job_obj))
-            job_obj.gc['picard params']['cmax'] = 200
-            submit_job(job_obj,'carlgladish@gmail.com','8:00:00','q',1,True)
-
 if __name__ == '__main__':
 
-    if (len(sys.argv) < 3):
-        print ("usage: process_jobs.py <jobprefix> <doRestarts>")
+    if (len(sys.argv) < 2):
+        print ("usage: process_jobs.py <jobprefix>")
     else:
-        doRestart = sys.argv[2].startswith('t') or sys.argv[2].startswith('T')
-
-        main(sys.argv[1],doRestart)
+        main(sys.argv[1])
