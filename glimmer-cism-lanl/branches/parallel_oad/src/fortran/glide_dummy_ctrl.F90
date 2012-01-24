6047da4f840316859fb227ed39cf7ff0efbfb642
utke <utke@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2012-01-24 20:14:03 +0000

    move the initializer definition here

--- glimmer-cism-lanl/branches/parallel_oad/src/fortran/glide_dummy_ctrl.F90
+++ glimmer-cism-lanl/branches/parallel_oad/src/fortran/glide_dummy_ctrl.F90
@@ -1,4 +1,15 @@
 module glide_dummy_ctrl
     use glimmer_global, only : dp 
     real(dp) inCtrl, outCtrl
-end module
+
+    contains
+      subroutine set_ctrl(model)
+        use glide_types
+        type(glide_global_type) :: model        ! model instance
+        ! stub for automatic differentiation
+        print *,"glide_dummy_ctrl:set_ctr; ", model%tempwk%inittemp(1,1,1), " perturbation:", inCtrl, &
+             " pertutrbed:", model%tempwk%inittemp(1,1,1)+inCtrl
+        model%tempwk%inittemp(1,1,1)=model%tempwk%inittemp(1,1,1)+inCtrl
+      end subroutine set_ctrl
+
+end module glide_dummy_ctrl
