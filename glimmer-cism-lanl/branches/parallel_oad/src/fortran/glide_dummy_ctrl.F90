7661c62c61ff47a0af1c2ab63712fa8cd2ebcf80
utke <utke@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2012-01-24 22:02:40 +0000

    fix the field being set and make the output show exactly the modified temp

--- glimmer-cism-lanl/branches/parallel_oad/src/fortran/glide_dummy_ctrl.F90
+++ glimmer-cism-lanl/branches/parallel_oad/src/fortran/glide_dummy_ctrl.F90
@@ -7,9 +7,10 @@ module glide_dummy_ctrl
         use glide_types
         type(glide_global_type) :: model        ! model instance
         ! stub for automatic differentiation
-        print *,"glide_dummy_ctrl:set_ctr; ", model%tempwk%inittemp(1,1,1), " perturbation:", inCtrl, &
-             " pertutrbed:", model%tempwk%inittemp(1,1,1)+inCtrl
-        model%tempwk%inittemp(1,1,1)=model%tempwk%inittemp(1,1,1)+inCtrl
+        print *,"glide_dummy_ctrl:set_ctr original value  :",  model%temper%temp(1,1,1)
+        print *,"glide_dummy_ctrl:set_ctr perturbation abs:",  inCtrl, " rel:", inCtrl/model%temper%temp(1,1,1)
+        model%temper%temp(1,1,1)= model%temper%temp(1,1,1)+inCtrl
+        print *,"glide_dummy_ctrl:set_ctr perturbed value : ", model%temper%temp(1,1,1)
       end subroutine set_ctrl
 
 end module glide_dummy_ctrl
