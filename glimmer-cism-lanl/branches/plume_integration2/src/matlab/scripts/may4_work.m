a298dfc464abb0fefb5545385b88773e6764eb35
cgladish <cgladish@2cfc7cc4-626f-0410-8325-f6cb4dace9bf>
2012-09-21 11:06:24 +0000

    updating plot scripts

--- glimmer-cism-lanl/branches/plume_integration2/src/matlab/scripts/may4_work.m
+++
@@ -1,6 +0,0 @@
-f = '/data/gladish/may3_coupled_super_channels_trial_90/plume.may3_coupled_super_channels_trial_90.out.nc';
-%f2 = '/data/gladish/may3_coupled_super_channels_trial_90/may3_coupled_super_channels_trial_90.out.nc';
-d = nc_plume_read(f);
-%d2 = nc_read(f2,-1);
-
-
